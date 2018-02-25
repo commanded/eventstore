6bfa698e1cbf4e2fda6a63f23ec05e879070ba1f
Ben Smith <ben@10consulting.com>
2018-02-25 16:15:16 +0000

    Fix broken benchmark suite
    Use default Postgrex config to reset storage for benchmark suite.

--- test/support/storage_initializer.ex
+++ test/support/storage_initializer.ex
@@ -2,7 +2,7 @@ defmodule EventStore.StorageInitializer do
   alias EventStore.Config
 
   def reset_storage! do
-    with {:ok, conn} <- Config.parsed() |> Postgrex.start_link() do
+    with {:ok, conn} <- Config.parsed() |> Config.default_postgrex_opts() |> Postgrex.start_link() do
       EventStore.Storage.Initializer.reset!(conn)
     end
 
