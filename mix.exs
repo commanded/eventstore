ea39e0305d6cd8b16060e49fc097ea79a8309fa7
Ben Smith <ben@10consulting.com>
2020-05-06 15:12:17 +0100

    Bump Highlander dependency

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule EventStore.Mixfile do
       {:fsm, "~> 0.3"},
       {:gen_stage, "~> 1.0"},
       {:postgrex, "~> 0.15"},
-      {:highlander, "~> 0.1.0"},
+      {:highlander, "~> 0.2"},
 
       # Optional dependencies
       {:jason, "~> 1.2", optional: true},
