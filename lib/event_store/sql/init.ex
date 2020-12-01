8895987d40ecb0bc17371d3eab5c5db247ba66d5
Derek Kraan <derek.kraan@gmail.com>
2020-12-01 12:06:05 +0100

    Fix the remaining two migrations tests

--- lib/event_store/sql/init.ex
+++ lib/event_store/sql/init.ex
@@ -267,7 +267,7 @@ defmodule EventStore.Sql.Init do
   defp record_event_store_schema_version do
     """
     INSERT INTO schema_migrations (major_version, minor_version, patch_version)
-    VALUES (1, 2, 0);
+    VALUES (1, 2, 1);
     """
   end
 end
